b0e3dcc7e89362d47f937497764ea1d5026f7f70
param-usc <51987353+param-b@users.noreply.github.com>
2022-09-11 22:25:32 -0700

    Garbage Collector Logic & Updated Sample Letter Logic

--- Assets/Scripts/Proto1 Scripts/letter.cs
+++ Assets/Scripts/Proto1 Scripts/letter.cs
@@ -11,10 +11,16 @@ public class letter : MonoBehaviour
     public Boolean displacementX = true;
     public float displacementParam1 = 0.3f;
     public float displacementParam2 = 0.1f;
+    private float initialPosition = 0.3f ;
 
        
     void Start()
     {
+        if (displacementX)
+            initialPosition = transform.position.x;
+        else
+            initialPosition = transform.position.y;
+        
         if (moving)
             StartCoroutine(RunLoop());
     }
@@ -27,7 +33,7 @@ public class letter : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D col)
     {
-        Debug.Log("Collision for bubble");
+        Debug.Log("Collision for Letter");
         Debug.Log(col.gameObject.tag);
         if (col.gameObject.tag == "Bullet")
         {
@@ -39,13 +45,14 @@ public class letter : MonoBehaviour
     private IEnumerator RunLoop() {
         for (float t=0f; ; t += Time.deltaTime) { 
             
+            Vector3 currenrtTransformPosition = transform.position;
+
             float currentDisplacement = Mathf.PingPong(Time.time * pingPongSpeed, 1) * displacementParam1 - displacementParam2;
 
-            Vector3 currenrtTransformPosition = transform.position;
             if (displacementX)
-                transform.position = new Vector3(currentDisplacement, currenrtTransformPosition.y, currenrtTransformPosition.z);
+                transform.position = new Vector3(  initialPosition + currentDisplacement, currenrtTransformPosition.y, currenrtTransformPosition.z);
             else
-                transform.position = new Vector3(currenrtTransformPosition.x, currentDisplacement, currenrtTransformPosition.z);
+                transform.position = new Vector3(currenrtTransformPosition.x,  initialPosition + currentDisplacement, currenrtTransformPosition.z);
             
             yield return null; // "wait for a frame"
         }
