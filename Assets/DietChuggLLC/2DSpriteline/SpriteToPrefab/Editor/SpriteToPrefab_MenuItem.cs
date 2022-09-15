a7fd4fd0638df2c4fc7937d5448e802501648d0e
param-usc <51987353+param-b@users.noreply.github.com>
2022-09-14 22:07:58 -0700

    Added Letter Pre-Fabs

---
+++ Assets/DietChuggLLC/2DSpriteline/SpriteToPrefab/Editor/SpriteToPrefab_MenuItem.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Linq;
+
+public class SpriteToPrefab_MenuItem
+{
+    /// <summary>
+    /// Creates Sprites To Prefab
+    /// </summary>
+    [MenuItem("Assets/Create/SpriteToPrefab", false, 11)]
+    public static void ScriptableObjectTemplateMenuItem()
+    {
+        bool makeSeperateFolders = EditorUtility.DisplayDialog("Prefab Folders?", "Do you want each prefab in it's own folder?", "Yes", "No");
+        for (int i = 0; i < Selection.objects.Length; i++)
+        {
+            string spriteSheet = AssetDatabase.GetAssetPath(Selection.objects[i]);
+            Sprite[] sprites = AssetDatabase.LoadAllAssetsAtPath(spriteSheet).OfType<Sprite>().ToArray();
+            string[] splitSpriteSheet = spriteSheet.Split(new char[] { '/' });
+            string fullFolderPath = Inset(spriteSheet, 0, splitSpriteSheet[splitSpriteSheet.Length - 1].Length + 1) + "/" + Selection.objects[i].name;
+            //string fullFolderPath = Inset(spriteSheet, 0, splitSpriteSheet[splitSpriteSheet.Length - 1].Length + 1) + "/" + (Char)(Convert.ToUInt16('A') + i);
+
+            string folderName = Selection.objects[i].name;
+            string adjFolderPath = InsetFromEnd(fullFolderPath, Selection.objects[i].name.Length + 1);
+
+            if (!AssetDatabase.IsValidFolder(fullFolderPath))
+            {
+                AssetDatabase.CreateFolder(adjFolderPath, folderName);
+            }
+
+            GameObject gameObject = new GameObject();
+            SpriteRenderer spriteRenderer = gameObject.AddComponent<SpriteRenderer>();
+            gameObject.AddComponent<Rigidbody2D>();
+            gameObject.AddComponent<BoxCollider2D>();
+            gameObject.tag = "Letter";
+            System.Type MyScriptType = System.Type.GetType ("Letters" + ",Assembly-CSharp");
+            //Now that we have the Type we can use it to Add Component
+            gameObject.AddComponent (MyScriptType);
+            for (int j = 0; j < sprites.Length; j++)
+            {
+                EditorUtility.DisplayProgressBar((i + 1).ToString() + "/" + Selection.objects.Length + " Generating Prefabs", "Prefab: " + j, (float)j / (float)sprites.Length);
+                gameObject.name = sprites[j].name;
+                spriteRenderer.sprite = sprites[j];
+
+                // string savePath = makeSeperateFolders
+                //     ? fullFolderPath + "/" + sprites[j].name + "/" + sprites[j].name + ".prefab"
+                //     : fullFolderPath + "/" + sprites[j].name + ".prefab";
+                string savePath = makeSeperateFolders ? fullFolderPath + "/" + (Char)(Convert.ToUInt16('A') + j-1) + "/" + (Char)(Convert.ToUInt16('A') + j-1) + ".prefab" : fullFolderPath + "/" + (Char)(Convert.ToUInt16('A') + j-1) + ".prefab";
+
+                if (makeSeperateFolders)
+                {
+                    if (!AssetDatabase.IsValidFolder(fullFolderPath + "/" + sprites[j].name))
+                    {
+                        AssetDatabase.CreateFolder(fullFolderPath, sprites[j].name);
+                    }
+
+                }
+                PrefabUtility.CreatePrefab(savePath, gameObject);
+            }
+            GameObject.DestroyImmediate(gameObject);
+        }
+        EditorUtility.ClearProgressBar();
+
+    }
+
+    /// <summary>
+    /// removes inset amounts from string ie. "0example01" with leftIn at 1 and with rightIn at 2 would result in "example"
+    /// </summary>
+    /// <param name="me"></param>
+    /// <param name="inset"></param>
+    /// <returns></returns>
+    public static string Inset(string me, int leftIn, int rightIn)
+    {
+        return me.Substring(leftIn, me.Length - rightIn - leftIn);
+    }
+
+    /// <summary>
+    /// removes inset amount from string end ie. "example01" with inset at 2 would result in "example"
+    /// </summary>
+    /// <param name="me"></param>
+    /// <param name="inset"></param>
+    /// <returns></returns>
+    public static string InsetFromEnd(string me, int inset)
+    {
+        return me.Substring(0, me.Length - inset);
+    }
+
+}
--- Assets/Scripts/Letters.cs
+++ Assets/Scripts/Letters.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,16 @@ using UnityEngine;
 public class Letters : MonoBehaviour
 {
     public float speed=4.0f;
+    public Boolean moving = true;
+    public float pingPongSpeed = 0.25f;
+    public Boolean displacementX = true;
+    public float displacementParam1 = 0.3f;
+    public float displacementParam2 = 0.1f;
+    private float initialPosition = 0.3f ;
+
     private Rigidbody2D rb;
     private Vector2 screenBounds;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +23,13 @@ public class Letters : MonoBehaviour
         rb=this.GetComponent<Rigidbody2D>();
         rb.velocity=new Vector2(0,-speed);
         screenBounds=Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        if (displacementX)
+            initialPosition = transform.position.x;
+        else
+            initialPosition = transform.position.y;
+        
+        if (moving)
+            StartCoroutine(RunLoop());
     } 
 
     // Update is called once per frame
@@ -23,4 +39,33 @@ public class Letters : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
+    
+    
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("Collision for Letter");
+        Debug.Log(col.gameObject.tag);
+        if (col.gameObject.tag == "Bullet")
+        {
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        }
+    }
+    
+    private IEnumerator RunLoop() {
+        for (float t=0f; ; t += Time.deltaTime) { 
+            
+            Vector3 currenrtTransformPosition = transform.position;
+
+            float currentDisplacement = Mathf.PingPong(Time.time * pingPongSpeed, 1) * displacementParam1 - displacementParam2;
+
+            if (displacementX)
+                transform.position = new Vector3(  initialPosition + currentDisplacement, currenrtTransformPosition.y, currenrtTransformPosition.z);
+            else
+                transform.position = new Vector3(currenrtTransformPosition.x,  initialPosition + currentDisplacement, currenrtTransformPosition.z);
+            
+            yield return null; // "wait for a frame"
+        }
+    }
+
 }
