697e20071e855312bda51edc8f2a5c7fe9e33ba9
param-usc <51987353+param-b@users.noreply.github.com>
2022-09-16 13:10:13 -0700

    Random Prefab Generator

--- Assets/Scripts/LetterPrefabGenerator.cs
+++ Assets/Scripts/LetterPrefabGenerator.cs
@@ -7,6 +7,8 @@ public class LetterPrefabGenerator : MonoBehaviour
     public GameObject letterPrefab;
     public float letterReAppearTime=4.0f;
     private Vector2 screenBounds;
+    private static System.Random random = new System.Random();
+
 
     // Start is called before the first frame update
     void Start()
@@ -14,10 +16,31 @@ public class LetterPrefabGenerator : MonoBehaviour
         screenBounds=Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         StartCoroutine(letterLoop());
     }
+    
+
+     private char getLetter()
+    {
+        // This method returns a random lowercase letter
+        // ... Between 'a' and 'z' inclusize.
+        int num = random.Next(0, 26); // Zero to 25
+        char let = (char)('A' + num);
+        return let;
+    }
 
     private void createLetters(){
-        GameObject letter=Instantiate(letterPrefab) as GameObject;
+        //GameObject letter=Instantiate(letterPrefab) as GameObject;
+        
+        
+        GameObject letter = Instantiate(Resources.Load("Letters/"+getLetter()) as GameObject);
+        //Debug.Log("Tag : " +letter.tag);
+        Debug.Log("Name : " + letter.name);
         letter.transform.position=new Vector2(Random.Range(-screenBounds.x, screenBounds.x), screenBounds.y);
+        
+        GameObject letter2 = Instantiate(Resources.Load("Letters/"+getLetter()) as GameObject);
+        //Debug.Log("Tag : " +letter.tag);
+        Debug.Log("Name : " + letter2.name);
+        letter2.transform.position=new Vector2(Random.Range(-screenBounds.x, screenBounds.x), screenBounds.y);
+
     }
 
     IEnumerator letterLoop(){
