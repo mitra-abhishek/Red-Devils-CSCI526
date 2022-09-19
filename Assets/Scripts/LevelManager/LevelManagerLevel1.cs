1ef4d71c12bf93bd4b80bcdb3d3f4fa7cd34f7d7
Siddhant Khurana <sk72840@usc.edu>
2022-09-18 18:21:50 -0700

    Intermediate Commit for matching letters to Blanks

--- Assets/Scripts/LevelManager/LevelManagerLevel1.cs
+++ Assets/Scripts/LevelManager/LevelManagerLevel1.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LevelManagerLevel1 : MonoBehaviour
 {
@@ -10,7 +11,9 @@ public class LevelManagerLevel1 : MonoBehaviour
     // Other Game parameters can be added here! like health, time, etc;
 
     public String levelWord = "";
-    
+    public List<TMP_Text> blankList = new List<TMP_Text>();
+    public GameObject blankPrefab;
+    public Transform blankHolder;
     //private static Dictionary<int, List<string>> all_level_words = new Dictionary<int, List<string>>();
     
     private static System.Random random = new System.Random();
@@ -42,15 +45,41 @@ public class LevelManagerLevel1 : MonoBehaviour
         
     }
 
+    void OnEnable ()
+    {
+        EventManager.StartListening ("test", SomeFunction);
+    }
+
+    void OnDisable ()
+    {
+        EventManager.StopListening ("test", SomeFunction);
+    }
+
+    void SomeFunction (Dictionary<string, object> message)
+    {
+        Debug.Log(message);
+        var val = (Collider2D)message["amount"];
+        Debug.Log ($"{val.name[0]} received test!");
+        Debug.Log ("Some Function was called!: ");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Initialise();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Initialise(){
+        for(int i = 0;i<3;i++)
+        {
+            GameObject blankHelper = Instantiate(blankPrefab,blankHolder,false);
+            blankList.Add(blankHelper.GetComponent<TMP_Text>());
+        }
     }
 }
