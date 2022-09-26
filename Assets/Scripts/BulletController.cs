e44a9df9e5264a18ee3b1e817453fbd78913adec
divyanshu-jalther <divyanshu.jalther@gmail.com>
2022-09-25 22:57:40 -0700

    Bullets count text

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class BulletController : MonoBehaviour
 {
     public int bulletsToShoot;
+    [SerializeField] Text bulletsCountText;
 
    public void setBullets(int bullets){
        bulletsToShoot=bullets;
@@ -16,5 +17,13 @@ public class BulletController : MonoBehaviour
 
    public void subtractBullet(){
         bulletsToShoot--;
+   }
+
+   void Update() {
+        bulletsCountText.text = GameManager.instance.bulletController.getBullets().ToString();
+        
+        if (GameManager.instance.bulletController.getBullets() <= 10 ){
+             bulletsCountText.color = Color.red;
+        }
    }
 }
