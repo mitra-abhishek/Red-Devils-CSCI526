2fd27faa14f577cc947301c898b9934dcbcba5a6
divyanshu-jalther <divyanshu.jalther@gmail.com>
2022-09-27 17:44:45 -0700

    Kept 3 words in level 1

--- Assets/Scripts/LevelManager/LevelManagerLevel1.cs
+++ Assets/Scripts/LevelManager/LevelManagerLevel1.cs
@@ -56,7 +56,7 @@ public class LevelManagerLevel1 : MonoBehaviour
 
         List<string> level_words = new List<string>
         {
-            "CAT", "DOG", "FOX", "BAT", "OWL", "ANT"
+            "CAT", "DOG", "FOX"
         };
         
         int index = random.Next(level_words.Count);
