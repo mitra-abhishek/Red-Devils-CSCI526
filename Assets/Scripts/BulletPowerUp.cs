efa8d174bd9437efd4757b889b1bf0d878fca50a
param-usc <51987353+param-b@users.noreply.github.com>
2022-10-02 18:47:37 -0700

    Added Bullet Powerup

--- Assets/Scripts/BulletPowerUp.cs
+++ Assets/Scripts/BulletPowerUp.cs
@@ -1,19 +1,21 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rock : MonoBehaviour
+public class BulletPowerUp : MonoBehaviour
 {
-
     private Rigidbody2D rb;
     private Vector2 screenBounds;
+
+    public float bulletPowerUpSpeed = 2.0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Bullet Powerup Created");
         rb=this.GetComponent<Rigidbody2D>();
-        rb.velocity=new Vector2(0,-GameManager.instance.RockSpeed);
+        // Change to add bullet speed if needed
+        rb.velocity=new Vector2(0,-bulletPowerUpSpeed);
         screenBounds=Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
     }
 
@@ -22,6 +24,14 @@ public class Rock : MonoBehaviour
     {
         if(transform.position.y<screenBounds.y*-1){
             Destroy(this.gameObject);
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("player"))
+        {
+            Debug.Log("Bullet Powerup added to Player");
         }
     }
 }
---
+++ Assets/Scripts/BulletPowerupPrefabGenerator.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPowerupPrefabGenerator : MonoBehaviour
+{
+    private Vector2 screenBounds;
+    private static System.Random random = new System.Random();
+
+    public GameObject bulletPowerupPrefab;
+    public float bulletPowerupReAppearTime=2.0f;
+    public int maxbulletPowerupAtTime = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds=Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(bulletPowerupLoop());
+    }
+    
+    private void createbulletPowerupsDelayed()
+    {
+        for (int i = 0; i < random.Next(1, maxbulletPowerupAtTime); i++)
+        {
+            StartCoroutine(createbulletPowerupssDelayedCoRoutine());
+            //StopCoroutine(createLettersDelayedCoRoutine());
+        }
+    }
+
+    void createbulletPowerups()
+    {
+        GameObject bulletPowerup=Instantiate(bulletPowerupPrefab) as GameObject;
+        bulletPowerup.transform.position=new Vector2(Random.Range(-screenBounds.x+0.5f, screenBounds.x-0.5f), screenBounds.y);
+    }
+
+    private IEnumerator createbulletPowerupssDelayedCoRoutine()
+    {
+        yield return new WaitForSeconds(0.2f*random.Next(1,10));
+        GameObject bulletPowerup=Instantiate(bulletPowerupPrefab) as GameObject;
+        bulletPowerup.transform.position=new Vector2(Random.Range(-screenBounds.x, screenBounds.x), screenBounds.y);
+    }
+
+    IEnumerator bulletPowerupLoop(){
+        while(true){
+            yield return new WaitForSeconds(bulletPowerupReAppearTime);
+            createbulletPowerupsDelayed();
+        }
+    }
+}
