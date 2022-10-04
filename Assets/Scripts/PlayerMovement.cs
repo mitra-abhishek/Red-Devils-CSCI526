cc9ef7c1743f923e0b2c8058327878819b875a26
mitra-abhishek <abhishek.96.mitra@gmail.com>
2022-10-04 16:58:30 -0700

    Rock speed and reapper time updated

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -48,7 +48,7 @@ public class PlayerMovement : MonoBehaviour
     void Update(){
         if (Input.GetKey(KeyCode.RightArrow)) {
             if(angle>= -90)
-            angle -= 1.7f;
+            angle -= 3.7f;
             transform.eulerAngles = new Vector3(0, 0, angle);
             print(angle);
         }
@@ -56,7 +56,7 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow)) {
             
             if(angle<= 90)
-            angle += 1.7f;
+            angle += 3.7f;
             transform.eulerAngles = new Vector3(0, 0, angle);
             print(angle);
         }
