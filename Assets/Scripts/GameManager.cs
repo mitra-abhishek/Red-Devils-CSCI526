d4c2834eb8cf2c0f2b2ae06ddfb353299101935a
mitra-abhishek <abhishek.96.mitra@gmail.com>
2022-10-11 16:57:04 -0700

    Fix for game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,24 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class GameManager : MonoBehaviour
 {  
     public static GameManager instance;
-
     public int Level = 1;
-
     public String LevelWord;
-
     public float LetterSpeed = 2.0f;
     
     public float RockSpeed = 2.0f;
-
     public int bullets;
-
     public BulletController bulletController;
-
-
+    public int ScreenDivisionNumber = 10;
+    private List<int> previousScreenPositionSelected = new List<int>(10);
+    
+    private static System.Random random = new System.Random();
     void Awake()
     {
         if (instance == null)
@@ -28,16 +24,42 @@ public class GameManager : MonoBehaviour
         }
         DontDestroyOnLoad(instance);
     }
-
     public void Start()
     {
         bulletController=new BulletController();
         bulletController.setBullets(bullets);
     }
-
+    private void OnDisable()
+    {
+        previousScreenPositionSelected = new List<int>(10);
+    }
+    private void OnEnable()
+    {
+        previousScreenPositionSelected = new List<int>(10);
+    }
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public float getRandomRange(float start, float end)
+    {
+        //float start = -screenBounds.x; float end = screenBounds.x;
+        start = start + 0.5f;
+        end = end - 0.5f;
+        float diff_per_screen = (end - start)/ScreenDivisionNumber;
+        int random_number = random.Next(ScreenDivisionNumber);
+        while (previousScreenPositionSelected.Contains(random_number))
+        {
+            random_number = random.Next(ScreenDivisionNumber);
+        }
+        if (previousScreenPositionSelected.Count >= 5)
+        {
+            previousScreenPositionSelected.RemoveAt(0);
+        }
+        
+        previousScreenPositionSelected.Add(random_number);
+        float final_position = start + random_number * diff_per_screen;
+        return final_position;
     }
 }
