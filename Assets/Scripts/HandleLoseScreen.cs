287fc888cac264dbe4ee8ce8bfc5da43e0048f12
prakash472 <prakashparajuli472@gmail.com>
2022-11-28 13:27:49 -0800

    Correct Word Implemented at Loss Screen

--- Assets/Scripts/HandleLoseScreen.cs
+++ Assets/Scripts/HandleLoseScreen.cs
@@ -5,11 +5,17 @@ using TMPro;
 public class HandleLoseScreen : MonoBehaviour
 {
     GameObject reasonOfDeath;
+    GameObject correctLevelWord;
+
     void Start()
     {
         reasonOfDeath = GameObject.FindWithTag("death_reason");
+        correctLevelWord = GameObject.FindWithTag("correct_word");
         TextMeshProUGUI reasonOfDeathGui = reasonOfDeath.GetComponent<TextMeshProUGUI>();
         reasonOfDeathGui.text = GameManager.instance.reasonOfDeath.ToString();
+        Debug.Log("The check" + GameManager.instance.LevelWord.ToUpper());
+        TextMeshProUGUI correctWordGui = correctLevelWord.GetComponent<TextMeshProUGUI>();
+        correctWordGui.text = "  Correct Word: " + GameManager.instance.LevelWord.ToUpper();
     }
 
     // Update is called once per frame
--- Assets/Scripts/LevelManager/TutorialLevelManager.cs
+++ Assets/Scripts/LevelManager/TutorialLevelManager.cs
@@ -347,12 +347,12 @@ public class TutorialLevelManager : MonoBehaviour
     {
         if (isEnemiesSpawnOver == false)
         {
-            yield return new WaitForSeconds(1.5f);
+            yield return new WaitForSeconds(1f);
             waitTimeForEnemies = 3f;
         }
         if (isEnemiesSpawnOver == true)
         {
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(1f);
             waitTimeForLetterse = 4f;
         }
     }
